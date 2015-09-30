bd4c71da6a9376f888d72226e11d21623f7fa525
Nicolas Lindae <nicolas@lindae.de>
2015-09-30 12:30:40 +0200

    Add blocks.c for implementation of move methods

---
+++ blocks.c
@@ -0,0 +1,17 @@
+#include "blocks.h"
+
+
+int go_down(tetromino * t, world * w){
+	
+	return 0;
+}
+
+int go_left(tetromino * t, world * w){
+	
+	return 0;
+}
+
+int go_right(tetromino * t, world * w){
+	
+	return 0;
+}
