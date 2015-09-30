9fcbcc6d755de210092f56832f875e146edce512
Nicolas Lindae <nicolas@lindae.de>
2015-09-30 12:44:57 +0200

    Add method to create tetrominos and macros for cells

--- blocks.c
+++ blocks.c
@@ -15,3 +15,17 @@ int go_right(tetromino * t, world * w){
 	
 	return 0;
 }
+
+
+tetromino * create_tetromino(int type, int x, int y){
+	tetromino * new_t=malloc(sizeof(tetromino));
+	
+	new_t->x=x;
+	new_t->y=y;
+	
+	switch(type){
+		
+	}
+	
+	return new_t;
+}
--- blocks.h
+++ blocks.h
@@ -1,4 +1,5 @@
 #include "colors.h"
+#include <stdint.h>
 
 #define block 1
 #define air 0
@@ -6,10 +7,10 @@
 #define width 10
 #define height 22 
 
-typedef struct{
-	int color;
-	char visible;	
-} cell;
+typedef uint32_t cell; // left 16 bit = visible?, right 16 bit = color, use macros below
+
+#define VISIBLE(c) (c&0xFFFF0000)
+#define COLOR(c) (c&0x0000FFFF)
 
 typedef struct{
 	cell field[width][height];
@@ -22,6 +23,9 @@ typedef struct{
 } tetromino;
 
 
+
 int go_down(tetromino * t, world * w);
 int go_left(tetromino * t, world * w);
 int go_right(tetromino * t, world * w);
+
+tetromino * create_tetromino(int type, int x, int y);
