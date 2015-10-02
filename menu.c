5b52676450fe4546b03f8b7a066b832cddfce48e
Fabian Bonk <fabian.bonk@bonkii.com>
2015-10-02 23:05:16 +0200

    improve menu drawing

--- menu.c
+++ menu.c
@@ -6,7 +6,9 @@ char* options[] = {
 
 void menu(int * input_key){
 	int selection = 0;
-	int key = -1;
+	int key = 0;
+	clear();
+	draw_menu(selection);
 	while(1){
 		key = *input_key;
 		if(key == -2){
@@ -14,9 +16,9 @@ void menu(int * input_key){
 		}else if (key != -1){
 			*input_key = -1;//input read->reset
 			switch(key){
-			case KEY_UP: 	selection = (selection+1)%ARRAY_SIZE(options);break;
-			case KEY_DOWN: 	selection = (selection-1)%ARRAY_SIZE(options);break;
-			case KEY_ENTER:	activate_selection();break;
+			case KEY_UP: 	selection = (selection-1)%ARRAY_SIZE(options);break;
+			case KEY_DOWN: 	selection = (selection+1)%ARRAY_SIZE(options);break;
+			case KEY_ENTER:	activate_selection(selection);break;
 			default: break;
 			}
 
@@ -25,16 +27,21 @@ void menu(int * input_key){
 	}
 }
 
-void activate_selection(){
-
+void activate_selection(int selection){
+	switch(selection){
+		case 0:break;//Play
+		case 1:break;//New Game
+		case 2:break;//Settings
+		case 3:break;//Exit
+		case 4:break;//spooky dont click
+	}
 }
 
 void draw_menu(int selection){
-	clear();
 	attrset(C(WHITE));
 	for(int i=0; i<ARRAY_SIZE(options); i++){
 		move(i,0);
-		if(selection==i){
+		if(selection == i){
 			attrset(C(YELLOW));
 			printw(options[i]);
 			attrset(C(WHITE));
--- menu.h
+++ menu.h
@@ -6,7 +6,7 @@
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
 void menu(int * input_key);
-void activate_selection();
-void draw_menu();
+void activate_selection(int selection);
+void draw_menu(int selection);
 
 //extern char* options[]; 
